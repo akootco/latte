dcc223d3cbca80f1c58d586d1af4ffd7264e2649
Akoot <akoot@akoot.me>
2025-02-14 18:28:08 -0500

    Added more extension functions & classes

---
+++ src/main/kotlin/co/akoot/plugins/latte/extensions/PlayerExtensions.kt
@@ -0,0 +1,68 @@
+package co.akoot.plugins.latte.extensions
+
+import co.akoot.plugins.bluefox.extensions.*
+import co.akoot.plugins.latte.Latte
+import org.bukkit.*
+import org.bukkit.entity.Player
+import java.io.File
+
+private const val LATTE_WORLD = "world"
+
+/**
+ * Applies the specified [world] settings on [this] player
+ * @param world The world to apply settings from
+ */
+fun Player.applySettings(world: World) {
+    if(hasPermission(Latte.Permission.GAMEMODE_BYPASS) && gameMode in setOf(GameMode.CREATIVE, GameMode.SPECTATOR)) return
+    gameMode = world.rootWorld.gameMode
+    if(world.rootWorld.flyMode) allowFlight = true
+}
+
+/**
+ * Get [this] offline player's data file from the specified [world]
+ * @param world The world to get the data file from
+ * @return The specified world's data file
+ */
+fun OfflinePlayer.getDataFile(world: World): File {
+    return world.getDataFile(this)
+}
+
+/**
+ * Loads the specified [world]'s data file onto [this] player's current data file
+ * @param world The world to load
+ */
+fun Player.loadData(world: World) {
+    val rootWorld = world.rootWorld
+    val dataFile = getDataFile(rootWorld)
+    if(!dataFile.exists()) createDataFile(rootWorld)
+    dataFile.copyTo(getDataFile(), true)
+}
+
+/**
+ * Saves [this] player's current data file in the specified [world]'s directory
+ * @param world The world to save
+ */
+fun Player.saveData(world: World) {
+    saveData()
+    getDataFile().copyTo(getDataFile(world.rootWorld), true)
+    setPDC(Latte.key(LATTE_WORLD), world.name)
+}
+
+/**
+ * The world the player was last teleported to.
+ */
+val Player.latteWorld: World? get() = getPDC<String>(Latte.key(LATTE_WORLD))?.let { Bukkit.getWorld(it) }
+
+/**
+ * Create a new data file for the specified [world]. This data file will copy [this] player's current
+ * data file but with a clear inventory & end chest, and 0 xp
+ * @param world The world
+ */
+fun Player.createDataFile(world: World) {
+    saveData(this.world)
+    inventory.clear()
+    enderChest.clear()
+    exp = 0f
+    totalExperience = 0
+    saveData(world)
+}
---
+++ src/main/kotlin/co/akoot/plugins/latte/extensions/WorldExtensions.kt
@@ -0,0 +1,92 @@
+package co.akoot.plugins.latte.extensions
+
+import co.akoot.plugins.bluefox.api.FoxConfig
+import co.akoot.plugins.bluefox.extensions.mkdirp
+import co.akoot.plugins.latte.extensions.WorldKeys.ALLOW_ADVANCEMENTS
+import co.akoot.plugins.latte.extensions.WorldKeys.ALLOW_FLIGHT
+import co.akoot.plugins.latte.extensions.WorldKeys.ALLOW_STATS
+import co.akoot.plugins.latte.extensions.WorldKeys.GAME_MODE
+import co.akoot.plugins.latte.extensions.WorldKeys.PARENT_WORLD
+import org.bukkit.*
+import org.bukkit.World.Environment
+import java.io.File
+
+
+object WorldKeys {
+    const val ENVIRONMENT = "environment"
+    const val TYPE = "type"
+    const val GAME_MODE = "gameMode"
+    const val ALLOW_FLIGHT = "allowFlight"
+    const val PARENT_WORLD = "parentWorld"
+    const val AUTO_LOAD = "autoload"
+    const val ALLOW_ADVANCEMENTS = "allowAdvancements"
+    const val ALLOW_STATS = "allowStats"
+
+    val booleans = setOf(AUTO_LOAD, ALLOW_FLIGHT, ALLOW_ADVANCEMENTS, ALLOW_STATS)
+    val enums = setOf(GAME_MODE, ENVIRONMENT, TYPE)
+    val all = setOf(PARENT_WORLD) + enums + booleans
+}
+
+val World.config: FoxConfig get() = FoxConfig(worldFolder.resolve("latte.conf"))
+val World.parentWorld: World? get() = config.getString(PARENT_WORLD)?.let { Bukkit.getWorld(it) }
+val World.rootWorld: World get() = parentWorld ?: getRelatedWorld(Environment.NORMAL) ?: this
+val World.gameMode: GameMode get() = config.getString(GAME_MODE)?.let { GameMode.valueOf(it.uppercase()) } ?: GameMode.SURVIVAL
+val World.flyMode: Boolean get() = config.getBoolean(ALLOW_FLIGHT) ?: false
+val World.statsAllowed: Boolean get() = config.getBoolean(ALLOW_STATS) ?: true
+val World.advancementsAllowed: Boolean get() = config.getBoolean(ALLOW_ADVANCEMENTS) ?: true
+
+/**
+ * Get the related world based on the specified [environment].
+ * If [this] world's name is "pepe", and [environment] is [Environment.THE_END], then the related world
+ * would be "pepe_the_end".
+ *
+ * @param environment The environment to check for
+ * @return The related world, if found, null otherwise
+ */
+fun World.getRelatedWorld(environment: Environment?): World? {
+    if (environment == null) return null
+    if (environment == this.environment) return null
+
+    val worldName = name
+    val relatedName = when (environment) {
+        Environment.NETHER -> if (worldName.endsWith("_nether")) worldName else "${worldName}_nether"
+        Environment.THE_END -> if (worldName.endsWith("_the_end")) worldName else "${worldName}_the_end"
+        Environment.NORMAL -> worldName.removeSuffix("_nether").removeSuffix("_the_end")
+        else -> return null
+    }
+
+    return Bukkit.getWorld(relatedName)
+}
+
+/**
+ * Check if either [this] world's parent world is [world], or [world]'s parent world is [this] world, OR they are related
+ * based on their environments
+ *
+ * @param world The world to check
+ * @return Whether [this] world is related to [world] in any way
+ */
+fun World.isRelated(world: World): Boolean {
+    return parentWorld == world || world.parentWorld == this || isRelatedEnvironment(world)
+}
+
+/**
+ * Check if [this] world is related to [world] based on their environments.
+ * If [this] world is of [Environment.NORMAL] and [world]'s environment is of [Environment.NETHER] AND [world]'s
+ * name ends with "_nether", then they are related.
+ *
+ * @param world The world to check
+ * @return Whether [this] world is related to [world] based on their environments
+ */
+fun World.isRelatedEnvironment(world: World): Boolean {
+    return getRelatedWorld(world.environment) == world
+}
+
+/**
+ * Get the [offlinePlayer]'s data file from [this] world
+ * @param offlinePlayer The offline player
+ * @return The data file for the offline player from [this] world
+ */
+fun World.getDataFile(offlinePlayer: OfflinePlayer): File {
+    return worldFolder.resolve("latte/playerdata").mkdirp()
+        .resolve("${offlinePlayer.uniqueId}.dat")
+}
